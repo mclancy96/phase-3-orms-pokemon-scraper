88ad315eb4cc5870f57c991af3f2019679774f82
Mike <mclancy96@gmail.com>
2025-07-23 14:36:03 -0400

    Find pokemon

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,4 +14,17 @@ class Pokemon
     SQL
     db.execute(sql, [name, type])
   end
+
+  def self.find(pokemon_id, db)
+    sql = <<-SQL
+      SELECT * FROM pokemon where id = ?
+    SQL
+    pokemon_from_db(db.execute(sql, [pokemon_id]).first, db)
+  end
+
+  def self.pokemon_from_db(attribute_array, db)
+    Pokemon.new(id: attribute_array[0], name: attribute_array[1], type: attribute_array[2], db: db)
+  end
+
+  private_class_method :pokemon_from_db
 end
